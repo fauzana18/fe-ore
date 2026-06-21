6f7309ebd278d6935ce1c0631558c96b694e8afe
Ahmad Fauzan Maulana <fauzana18@gmail.com>
2026-06-21 22:51:09 +0700

    tuning ui

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,6 @@
-<script setup>
-// import router from '@/router/index';
-import { categoryStore, profileStore, saldoStore, planStore } from '@/store/finance';
-import { useEncryptor } from '@/composables/encryptor';
-import { onMounted } from 'vue';
-
-const { deriveVaultKey } = useEncryptor()
-const profiles = profileStore()
-const category = categoryStore()
-const saldo = saldoStore()
-const plan = planStore()
-
-onMounted(async () => {
-    // const main_path = router.options.routes[0].children.map(route => route.path)
-    
-    // if(main_path.includes(window.location.pathname) || window.location.pathname == '/') {
-        
-    // }
-    // const init = await Promise.all([
-    //     profiles.getList(),
-    //     category.getList(),
-    //     plan.getPlanNextMonth(),
-    //     deriveVaultKey(import.meta.env.VITE_PASSWORD, import.meta.env.VITE_SALT)
-    // ])
-    deriveVaultKey(import.meta.env.VITE_PASSWORD, import.meta.env.VITE_SALT)
-    await profiles.getList()
-    await category.getList()
-    await plan.getPlanNextMonth()
-    await saldo.getSaldo(profiles.list[profiles.selected].id)
-    // vaultKey.setValue(key)
-})
-</script>
+<script setup></script>
 
 <template>
-    <!-- <div v-if="loading" class="fixed inset-0 z-[9999] flex flex-col items-center justify-center bg-black/20 backdrop-blur-sm"> -->
-        <!-- <div class="rounded-xl p-6 shadow-lg">
-        </div> -->
-        <!-- <div class="w-60 h-60 border-20 border-primary-200 border-t-primary-500 rounded-full animate-spin" />
-        <span class="text-lg font-medium mt-4">Memuat data</span>
-    </div> -->
     <router-view />
 </template>
 
--- src/layout/AppLayout.vue
+++ src/layout/AppLayout.vue
@@ -1,10 +1,17 @@
 <script setup>
 import { useLayout } from '@/composables/layout';
-import { computed } from 'vue';
+import { computed, onMounted } from 'vue';
 import AppFooter from './AppFooter.vue';
 import AppSidebar from './AppSidebar.vue';
 import AppTopbar from './AppTopbar.vue';
+import { categoryStore, profileStore, saldoStore, planStore } from '@/store/finance';
+import { useEncryptor } from '@/composables/encryptor';
 
+const { deriveVaultKey } = useEncryptor()
+const profiles = profileStore()
+const category = categoryStore()
+const saldo = saldoStore()
+const plan = planStore()
 const { layoutConfig, layoutState, openMobileMenu, hideMobileMenu } = useLayout();
 
 const containerClass = computed(() => {
@@ -16,13 +23,27 @@ const containerClass = computed(() => {
         'layout-static-inactive': layoutState.staticMenuInactive
     };
 });
+
+onMounted(async () => {
+    deriveVaultKey(import.meta.env.VITE_PASSWORD, import.meta.env.VITE_SALT)
+    await profiles.getList()
+    await category.getList()
+    await plan.getPlanNextMonth()
+    await saldo.getSaldo(profiles.list[profiles.selected].id)
+})
 </script>
 
 <template>
+    <!-- <div v-if="loading" class="fixed inset-0 z-[9999] flex flex-col items-center justify-center bg-black/20 backdrop-blur-sm"> -->
+        <!-- <div class="rounded-xl p-6 shadow-lg">
+        </div> -->
+        <!-- <div class="w-60 h-60 border-20 border-primary-200 border-t-primary-500 rounded-full animate-spin" />
+        <span class="text-lg font-medium mt-4">Memuat data</span>
+    </div> -->
     <div class="layout-wrapper" :class="containerClass" v-touch:swipe.right="openMobileMenu">
         <AppTopbar />
         <AppSidebar />
-        <div class="layout-main-container">
+        <div class="layout-main-container !px-[5px] md:!px-8">
             <div class="layout-main">
                 <router-view />
             </div>
